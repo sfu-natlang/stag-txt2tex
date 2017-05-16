3aca494084344c2dbfa794d354064a9e385b3e10
Anoop Sarkar <anoop+github@cs.sfu.ca>
2017-05-16 00:01:21 -0700

    new mcset macros added

--- txt2tex.py
+++ txt2tex.py
@@ -2,10 +2,10 @@
 # - generate all trees in one forest environment? then adjunction arrows are easier to draw using tikz named nodes
 # - for STAG pairs: find bottom-left corner of source, bottom right corner of target, and give them special labels. then draw STAG brackets/MCsets with a method like the following:
 #
-#	\path[draw=none] let \p1=(bottomleft.south), \p2=(bottomright.south) in (bottomright.east|-\pgfmath{min(\y1, \y2)}) -- coordinate[midway](right1) (bottomright.east|-srcroot.north);
-#	\path[draw=none] let \p1=(bottomleft.south), \p2=(bottomright.south) in (bottomleft.west|-\pgfmath{min(\y1,\y2)}) -- coordinate[midway](left1) (bottomleft.west|-srcroot.north);
-#	\draw let \p1=(srcroot.north), \p2=(bottomleft.south), \p3=(bottomright.south), \n1={(\y1-min(\y2,\y3)) / 2} in (left1) node {\resizebox{\abw}{\n1}{$\biggl<$}};
-#	\draw let \p1=(srcroot.north), \p2=(bottomleft.south), \p3=(bottomright.south), \n1={(\y1-min(\y2,\y3)) / 2} in (right1) node {\resizebox{\abw}{\n1}{$\biggr>$}};
+#       \path[draw=none] let \p1=(bottomleft.south), \p2=(bottomright.south) in (bottomright.east|-\pgfmath{min(\y1, \y2)}) -- coordinate[midway](right1) (bottomright.east|-srcroot.north);
+#       \path[draw=none] let \p1=(bottomleft.south), \p2=(bottomright.south) in (bottomleft.west|-\pgfmath{min(\y1,\y2)}) -- coordinate[midway](left1) (bottomleft.west|-srcroot.north);
+#       \draw let \p1=(srcroot.north), \p2=(bottomleft.south), \p3=(bottomright.south), \n1={(\y1-min(\y2,\y3)) / 2} in (left1) node {\resizebox{\abw}{\n1}{$\biggl<$}};
+#       \draw let \p1=(srcroot.north), \p2=(bottomleft.south), \p3=(bottomright.south), \n1={(\y1-min(\y2,\y3)) / 2} in (right1) node {\resizebox{\abw}{\n1}{$\biggr>$}};
 #
 
 import sys, re
@@ -18,20 +18,48 @@ parser.add_argument('-f', dest='path', help='Path to file containing trees.' )
  
 args = parser.parse_args()
 
-def parseTree( string ):
-  return parseTree_( string, 0, '' )
+def parseNamedTree( s ):
+  s = s.strip()
+  if s[0] == '{':
+    return ( ('',) + parseMCSet( s ) )
+  elif s[0] == '[':
+    return ( ('',) + parseTree( s ) )
+  else:
+    name, rest = s.split(' ', 1)
+    return ( (name,) + parseTree( rest ) )  
 
-def parseTree_( string, openBraces, tree ):
-  char = string[0]
+def parseMCSet( s ):
+  if s[0] != '{':
+    raise ValueError("bad input for MCSet")
+  rest = s[1:]
+  (left_name, rest, left_tree) = parseNamedTree(rest)
+  (right_name, rest, right_tree) = parseNamedTree(rest)
+  rest = rest.strip()
+  if rest[0] != '}':
+    raise ValueError("bad input for MCSet")
+  rest = rest[1:] # remove closing curly bracket
+  mcset = ''
+  if left_name is not '' and right_name is not '':
+      mcset = r'\namedmcset{'+left_name+'}{'+left_tree+'}{'+right_name+'}{'+right_tree+'}'
+  else:
+      mcset = r'\mcset{'+left_tree+'}{'+right_tree+'}'
+  return (rest, mcset)
+
+
+def parseTree( s ):
+  return parseTree_( s, 0, '' )
+
+def parseTree_( s, openBraces, tree ):
+  char = s[0]
   if char == '[':
-    return parseTree_( string[1:], openBraces+1, tree+char )
+    return parseTree_( s[1:], openBraces+1, tree+char )
   elif char == ']':
     if openBraces-1 == 0:
-      return (string[1:], tree+char)
+      return (s[1:], tree+char)
     else:
-      return parseTree_( string[1:], openBraces-1, tree+char )
+      return parseTree_( s[1:], openBraces-1, tree+char )
   else:
-    return parseTree_( string[1:], openBraces, tree+char )
+    return parseTree_( s[1:], openBraces, tree+char )
 
 preamble = """
 \\usepackage{adjustbox}
@@ -43,7 +71,7 @@ preamble = """
 \\renewcommand{\\boxed}[1]{\\raisebox{1pt}{\\scalebox{0.65}{\\fbox{#1}}}}
 
 
-% (Adapted from) Dennis's STAG angle brackets:
+% Draw the angled brackets around the STAG tree-pair
 \\newlength{\\abw}
 \\settowidth{\\abw}{$\\biggl<$}
 \\newsavebox{\\mybox}
@@ -52,6 +80,17 @@ preamble = """
     \\resizebox{\\abw}{\\ht\\mybox}{$\\biggl<$}%
     \\raisebox{.25\\ht\\mybox}{\\usebox{\\mybox}}%
     \\resizebox{\\abw}{\\ht\\mybox}{$\\biggr>$}%
+}
+
+% Draw the curly brackets around the STAG MC sets
+\\newlength{\\acw}
+\\settowidth{\\acw}{$\\biggl\\{$}
+\\newsavebox{\\cbox}
+\\newcommand{\\bigacx}[1]{%
+    \\savebox{\\cbox}{#1}%
+    \\resizebox{\\acw}{\\ht\\cbox}{$\\biggl\\{$}%
+    \\raisebox{.25\\ht\\cbox}{\\usebox{\\cbox}}%
+    \\resizebox{\\acw}{\\ht\\cbox}{$\\biggr\\}$}%
 }
 
 % STAG tree-pair
@@ -71,15 +110,96 @@ preamble = """
     }
 }
 
+% Named STAG tree-pair
+\\newcommand{\\namedstagrule}[4]{
+    \\bigabx{
+        \\begin{adjustbox}{valign=M}
+            \\begin{forest}
+            [,phantom,s sep=0
+                [#1:]
+                #2
+            ]
+            \\end{forest}
+        \\end{adjustbox}
+        ,
+        \\begin{adjustbox}{valign=M}
+            \\begin{forest}
+            [,phantom,s sep=0
+                [#3:]
+                #4
+            ]
+            \\end{forest}
+        \\end{adjustbox}
+    }
+}
+
+% Named STAG MCSet tree-pair
+\\newcommand{\\namedstagmcset}[3]{
+    \\bigabx{
+        \\begin{adjustbox}{valign=M}
+            \\begin{forest}
+            [,phantom,s sep=0
+                [#1:]
+                #2
+            ]
+            \\end{forest}
+        \\end{adjustbox}
+        ,
+        \\begin{adjustbox}{valign=M}
+            #3
+        \\end{adjustbox}
+    }
+}
+
+% STAG MCSet pair
+\\newcommand{\\mcset}[2]{
+    \\bigacx{
+        \\begin{adjustbox}{valign=M}
+            \\begin{forest}
+            #1
+            \\end{forest}
+        \\end{adjustbox}
+        ,
+        \\begin{adjustbox}{valign=M}
+            \\begin{forest}
+            #2
+            \\end{forest}
+        \\end{adjustbox}
+    }
+}
+
+% Named STAG MCSet pair
+\\newcommand{\\namedmcset}[4]{
+    \\bigacx{
+        \\begin{adjustbox}{valign=M}
+            \\begin{forest}
+            [,phantom,s sep=0
+                [#1:]
+                #2
+            ]
+            \\end{forest}
+        \\end{adjustbox}
+        ,
+        \\begin{adjustbox}{valign=M}
+            \\begin{forest}
+            [,phantom,s sep=0
+                [#3:]
+                #4
+            ]
+            \\end{forest}
+        \\end{adjustbox}
+    }
+}
+
 % Forest format: makes branches connect at the bottom
 % of a node:
 \\forestset{
     default preamble={
-		for tree={
-			parent anchor=south,
-			child anchor=north,
-			align=center,
-		}
+                for tree={
+                        parent anchor=south,
+                        child anchor=north,
+                        align=center,
+                }
     }
 }
 """
@@ -93,11 +213,17 @@ if args.preamble:
 
 while s.strip() != '':
   try:
-    (s,src_tree) = parseTree(s)
-    (s,tgt_tree) = parseTree(s)
+    (src_name, s, src_tree) = parseNamedTree(s)
+    (tgt_name, s, tgt_tree) = parseNamedTree(s)
   except IndexError:
     print('Couldn\'t parse tree! Are there are even number of trees in the input file?')
   link_annotation = r"\\circled{\1}" if args.circle else r"\\boxed{\1}"
   src_tree = re.sub( r"\((\d+)\)", link_annotation, src_tree.strip() )
   tgt_tree = re.sub( r"\((\d+)\)", link_annotation, tgt_tree.strip() )
-  print( "\stagrule{"+src_tree+"}{"+tgt_tree+"}" )
+  if src_name != '' and tgt_name != '':
+      print( r'\namedstagrule{'+src_name+'}{'+src_tree+'}{'+tgt_name+'}{'+tgt_tree+'}' )
+  elif src_name != '' and tgt_name == '':
+      print( r'\namedstagmcset{'+src_name+'}{'+src_tree+'}{'+tgt_tree+'}' )
+  else:
+      print( r'\stagrule{'+src_tree+'}{'+tgt_tree+'}' )
+
